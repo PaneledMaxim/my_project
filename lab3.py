493c254748f8fdca67dbb0f2e9ce8b2c0d94a151
Maxim Gurtov <maks.gurtov.00@mail.ru>
2025-10-09 08:39:14 +0300

    заданиe3

--- lab3.py
+++ lab3.py
@@ -5,3 +5,8 @@
 # 2.
 # even_numbers = [x for x in range(1, 20) if x % 2 == 0]
 # print(even_numbers)
+
+# 3.
+# words = ["python", "Java", "c++", "dog", "Rust", "go"]
+# result = [word.upper() for word in words if len(word) >= 3]
+# print(result)
